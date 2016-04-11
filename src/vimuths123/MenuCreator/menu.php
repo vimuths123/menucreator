359d239f39ecfe7a41a9c3d351ad15b27d7b71c5
vimuths123 <vimuths@yahoo.com>
2016-04-12 01:03:20 +0530

    Update menu.php

--- src/vimuths123/MenuCreator/menu.php
+++ src/vimuths123/MenuCreator/menu.php
@@ -1,1 +1,14 @@
-
+<?php
+namespace vimuths123\MenuCreator;
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class Menu
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
